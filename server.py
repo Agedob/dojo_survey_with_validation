cb8e518f21dca570d83921fda4651545b741453e
agedob <agedob888@gmail.com>
2018-02-12 20:59:06 -0800

    done.

--- server.py
+++ server.py
@@ -1,16 +1,24 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect, session , flash
+import random
 app = Flask(__name__)
+app.secret_key = "notthekey"
+
 @app.route('/')
 def index():    
     return render_template("index.html")
 
-@app.route('/proccess', methods=['POST'])
+@app.route('/process', methods=['POST'])
 def redirect_process():
     abc = request.form['name']
     abcd = request.form['location']
     abcde = request.form['lang']
     abcdef = request.form['updown']
-    return render_template("/proccess.html", name = abc, location = abcd, lang = abcde, updown = abcdef)
+    if len(abc) < 1 or len(abcd) < 1 or len(abcde) < 1 or len(abcdef) < 1 and len(abcdef) < 120:
+        flash("please fillout form")
+        return redirect('/')
+    else:
+        print "that was easy enough"
+        return render_template("/process.html", name = abc, location = abcd, lang = abcde, updown = abcdef)
 
    
 app.run(debug=True)
